4296a84d97388d913d6e41b9657ebd676a1da493
isidro lucena santiago <darksirius91@gmail.com>
2017-03-23 10:53:14 +0100

    mapa creado

--- dwec/geolocation/geoloc.js
+++ dwec/geolocation/geoloc.js
@@ -7,8 +7,11 @@ function getLocation() {
     }
 }
 function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude; 
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+
+    var img_url = "https://maps.googleapis.com/maps/api/staticmap?center="
+    +latlon+"&zoom=14&size=400x300&sensor=false&key=AIzaSyDRmKnFcgFiGVotXptU8ttZ_hTCNvDiRJE";
+    document.getElementById("mapholder").innerHTML = "<img src='"+img_url+"'>";
 }
 function showError(error) {
     switch(error.code) {
