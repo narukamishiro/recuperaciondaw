e5328b00a9dab792b33194d6c5372b55cbfb69ed
isidro lucena santiago <darksirius91@gmail.com>
2017-03-23 12:22:44 +0100

    ejercicios 7 y 8 completados

--- dwec/practica array/rep1.js
+++ dwec/practica array/rep1.js
@@ -25,15 +25,42 @@ function multArray(pk){
 function invertir(pk){
 	var ab=[];
 	var ai=[];
-	var l="";
-	ab=pk;
+	var l=" ";
+	ab=pk.split("");
 	var tam=ab.length;
 	for(i=tam-1;i>=0;i--){
 		l=ab[i];
 		ai.push(l);
 	}
-	return ai;
+	rs=ai.toString();
+	return rs;
 }
+function FindHugePhase(pk){
+	var mx=0;
+	var mp="";
+	ar=pk;
+	for(i=0;i<ar.length;i++){
+		p=ar[i];
+		if (p.length>mx){
+			mx=p.length;
+			mp=p;
+		}
+	}
+	return "la mayor palabra de ese array es "+mp+" con un total de "+mx+" letras";
+}
+function filtro(pk,tam){
+	var mn=tam;
+	var mp=[];
+	ar=pk;
+	for(i=0;i<ar.length;i++){
+		p=ar[i];
+		if (p.length>=mn){
+			mp.push(p);
+		}
+	}
+	return mp;
+}
+
 //ejercicio 3
 /*var m=new Array(2,6,3,10,9);
 document.getElementById("p").innerHTML=maxArray(m)+"";
@@ -43,6 +70,14 @@ document.getElementById("p").innerHTML=maxArray(m)+"";
 document.getElementById("p").innerHTML=multArray(m)+"";
 */
 //ejercicio 6
-var m="es una prueba";
+/*var m="es una prueba";
 document.getElementById("p").innerHTML=invertir(m)+"";
+*/
+//ejercicio 7
+/*var m=["moro","tolma","reck"];
+document.getElementById("p").innerHTML=FindHugePhase(m)+"";*/
+//ejercicio 8
+/*var m=["moro","tolma","reck","trimno"];
+document.getElementById("p").innerHTML=filtro(m,5)+"";*/
+
 
