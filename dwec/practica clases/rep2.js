4a8f89edf602c64421947bec6d2688d3572a4776
isidro lucena santiago <darksirius91@gmail.com>
2017-03-24 13:19:47 +0100

    mejora v1 clase piedra papel tijera

--- dwec/practica clases/rep2.js
+++ dwec/practica clases/rep2.js
@@ -2,45 +2,51 @@ class ppt{
 	var iach;
 	var pych;
 	var rnd;
+	constructor(){
+		this.pych="";
+		this.rnd=0;
+		this.iach="";
+	}
 	constructor(pl){
 		this.pych=pl;
 		this.rnd=Math.random()*9+1;
+		this.iach="";
 	}
 	iarnd(){
 		while(rnd>3){
 			rnd=rnd-3;
 		}
 		switch(rnd){
-			case 1:aich="piedra";break();
-			case 2:aich="papel";break();
-			case 3:aich="tijera";break();
+			case 1:iach="piedra";break();
+			case 2:iach="papel";break();
+			case 3:iach="tijera";break();
 		}
 	}
 	getResult(){
 		pl=pych.toLowerCase();
 		if(pl=="piedra"){
-			if(aich=="tijera"){
+			if(iach=="tijera"){
 				return "has ganado";
 			}else{
-				if(aich=="papel"){
+				if(iach=="papel"){
 					return "has perdido";
 				}else{return "empate"}
 			}
 		}
 		if(pl=="tijera"){
-			if(aich=="papel"){
+			if(iach=="papel"){
 				return "has ganado";
 			}else{
-				if(aich=="piedra"){
+				if(iach=="piedra"){
 					return "has perdido";
 				}else{return "empate"}
 			}
 		}
 		if(pl=="papel"){
-			if(aich=="piedra"){
+			if(iach=="piedra"){
 				return "has ganado";
 			}else{
-				if(aich=="tijera"){
+				if(iach=="tijera"){
 					return "has perdido";
 				}else{return "empate"}
 			}
@@ -50,4 +56,5 @@ class ppt{
  //ejercicio 1 y 2
  var pl=prompt("¿piedra,papel o tijeras? escoge una,con letras minusculas a poder ser,gracias");
  var ppt=new ppt(pl);
+ ppt.iarnd();
  document.getElementById("p").innerHTML=ppt.getResult();
